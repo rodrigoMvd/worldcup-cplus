82ab48299a8dcbf082607ee0848c7e51fb91aa15
Diego Maye <diegomaye@gepardo.local>
2018-07-22 11:23:00 -0300

    Initial Commit

---
+++ worldcup-cplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  worldcup-cplus
+//
+//  Created by diegomaye on 7/22/18.
+//  Copyright © 2018 Diego, Rodrigo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
